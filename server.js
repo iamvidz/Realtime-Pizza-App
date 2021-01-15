968eaa5ab1d3c357686eeb60d426d72d56ffe88e
iamvidz <vim7127@gmail.com>
2021-01-15 21:45:45 +0530

    Cart page and auth

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 3000
 //Assets
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.render('home')
-})
 
 //set template engine
 app.use(expressLayout)
@@ -19,4 +16,20 @@ app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
     console.log(`Listening on port test ${PORT}`)
+})
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req,res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req,res) =>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req,res) =>{
+    res.render('auth/register')
 })
