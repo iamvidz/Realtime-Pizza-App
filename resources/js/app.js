93d3f855b3b46e2cff44abe95117a080c92a9a55
iamvidz <vim7127@gmail.com>
2021-01-29 23:00:33 +0530

    add to cart using sessions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-console.log('hello from app.js')
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+    //ajax call
+    axios.post('/update-cart',pizza).then(res => {
+        console.log(res)
+        cartCounter.innerText = res.data.totalQty
+
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Item added to cart :)'
+        }).show();
+    }).catch( err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Something went wrong'
+        }).show();
+    })
+}
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        let pizza = JSON.parse(btn.dataset.pizza)
+        //console.log(pizza)
+        updateCart(pizza)
+    })
+})
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
@@ -5,6 +6,9 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
 
 //Database connection
 //const url = 'mongodb://vidhi-shard-00-00.bxchf.mongodb.net:27017/pizza';
@@ -23,9 +27,32 @@ connection.once('open',() =>{
     console.log('Connection failed...')
 });
 
+//session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000*60*60*24 } //24 hrs 
+}))
+
+app.use(flash())
+
 //Assets
 app.use(express.static('public'))
+app.use(express.json())
 
+//Global middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session
+    next()
+})
 
 //set template engine
 app.use(expressLayout)
