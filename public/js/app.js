36c40c43372c4109d1e97cfad69aa0e030f65584
harshjoshei <54684377+harshjoshei@users.noreply.github.com>
2021-01-30 12:26:47 +0530

    Authentication using passport

--- public/js/app.js
+++ public/js/app.js
@@ -5336,8 +5336,8 @@ addToCart.forEach(function (btn) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\SDP pizza\realtime-pizza\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\SDP pizza\realtime-pizza\resources\scss\app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! D:\backup2 sdp\realtime-pizza\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\backup2 sdp\realtime-pizza\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
